b4d6da552e927ebd11e62f4dd16caf26ff84a201
[K1mJongSeong] <miaer456@naver.com>
2023-04-25 16:45:04 +0900

    feat. 매일경제 크롤링 추가

--- BE/cominfoApp/views.py
+++ BE/cominfoApp/views.py
@@ -1,16 +1,15 @@
-from django.shortcuts import render
-from django.http import HttpResponse
-from .crawling import crawl_articles
+#from django.shortcuts import render
+from django.http import JsonResponse
+from .crawling import schedule_crawling
 
 def run_crawling(request):
-    # 크롤링 실행에 필요한 인자들
-    company_name = "삼성전자"
-    start_date = "20230401"
-    end_date = "20230413"
-
-    # 크롤링 실행
-    crawl_articles(company_name, start_date, end_date)
-
-    # HTTP 응답
-    return HttpResponse("Crawling completed.")
+    company_list = ["이아이디", "이브이첨단소재", "에코프로", "현대로템","에코프로비엠","피엔티","엘엔에프","이수화학","두산에너빌리티","이트론"]
+    schedule_crawling(company_list)
+    
+    response_data = {
+        'status': 'success',
+        'message': 'Crawling completed'
+    }
+    
+    return JsonResponse(response_data)
 
--- BE/my_settings.py
+++ BE/my_settings.py
@@ -6,7 +6,7 @@ DATABASES = {
         'PASSWORD': 'rldjqwjdqh', 
         'HOST': '116.124.133.159', 
         'PORT': '3306',
-        'CONN_MAX_AGE': 300,
+        'CONN_MAX_AGE': 300, #ORM 쿼리 실행시간 증가.
         'OPTIONS': {
             'init_command': "SET sql_mode='STRICT_TRANS_TABLES'"
         }
