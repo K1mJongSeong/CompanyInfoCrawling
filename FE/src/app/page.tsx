16e2901b602a615558a4d80a5e6970183215bc85
juneyoung <jadw951623@gmail.com>
2023-05-03 12:03:36 +0900

    setting. FE setting

---
+++ FE/src/app/page.tsx
@@ -0,0 +1,12 @@
+"use client";
+import {
+  Button,
+  Container,
+  Grid,
+  Stack,
+  Typography,
+} from "../components/@mui/material";
+
+export default function Home() {
+  return <div></div>;
+}
