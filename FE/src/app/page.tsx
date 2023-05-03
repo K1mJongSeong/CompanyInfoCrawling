e32a93bea503398f653c0e63fe15f9e6110a6e47
juneyoung <jadw951623@gmail.com>
2023-05-03 11:46:02 +0900

    setting. next setting

---
+++ FE/src/app/page.tsx
@@ -0,0 +1,19 @@
+"use client";
+import {
+  Button,
+  Container,
+  Grid,
+  Stack,
+  Typography,
+} from "../components/@mui/material";
+
+export default function Home() {
+  return (
+    <div>
+      이것은?
+      <Container>
+        <Typography variant="h1">HELLO WORLD!</Typography>
+      </Container>
+    </div>
+  );
+}
