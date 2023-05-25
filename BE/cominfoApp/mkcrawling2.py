c17fbed1fd90b22d01d20d7182d69c012af43649
[K1mJongSeong] <miaer456@naver.com>
2023-05-25 12:44:38 +0900

    feat. 각 크롤링마다 스케줄링 코드 추가(매월 1일마다 크롤링 시작)단, 크롤링 시 DB에 이미 있는 데이터는 다시 저장 하지 않음.

--- BE/cominfoApp/mkcrawling2.py
+++ BE/cominfoApp/mkcrawling2.py
@@ -8,141 +8,160 @@ from datetime import datetime,timedelta
 from newspaper import Article
 from newspaper.article import ArticleException
 from googletrans import Translator
+from django.core.exceptions import ObjectDoesNotExist
 from .models import Mkcrawling
+import schedule
 import time
 import nltk
 
 
 nltk.download('punkt')
 
+def first_day_of_month():
+    return datetime.now().day == 1
+
 def start_mk2():
-    translator = Translator()
-    companies = ["넷플릭스", "삼성전자", "LG전자"]
-    keywords = [["경찰조사수사","검찰조사수사","횡령배임","증여","사기","자금세탁"], 
-                ["법원소송","법원판결","공정거래위원회처분시정제재","금융감독원처분행정지도"], 
-                ["친환경에너지기술","탄소배출권","윤리경영","사회공헌","기업지배구조"]]
+    if first_day_of_month():
+        translator = Translator()
+        companies = ["넷플릭스", "삼성전자", "LG전자"]
+        keywords = [["경찰조사수사","검찰조사수사","횡령배임","증여","사기","자금세탁"], 
+                    ["법원소송","법원판결","공정거래위원회처분시정제재","금융감독원처분행정지도"], 
+                    ["친환경에너지기술","탄소배출권","윤리경영","사회공헌","기업지배구조"]]
 
 
-    today = datetime.now()
-    yesterday = today - timedelta(days=1)
-    yesterday_str = yesterday.strftime("%Y-%m-%d")
-    base_url = "https://www.mk.co.kr/search?word={company}{keyword}"#&dateType=1day&startDate={startDate}&endDate={endDate}
+        today = datetime.now()
+        yesterday = today - timedelta(days=1)
+        yesterday_str = yesterday.strftime("%Y-%m-%d")
+        base_url = "https://www.mk.co.kr/search?word={company}{keyword}"#&dateType=1day&startDate={startDate}&endDate={endDate}
 
 
-    chrome_options = webdriver.ChromeOptions()
-    chrome_options.add_argument("user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/89.0.4389.82 Safari/537.36")
-    chrome_options.add_argument('--ignore-certificate-errors')  # 인증서 오류 무시 옵션 추가
+        chrome_options = webdriver.ChromeOptions()
+        chrome_options.add_argument("user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/89.0.4389.82 Safari/537.36")
+        chrome_options.add_argument('--ignore-certificate-errors')  # 인증서 오류 무시 옵션 추가
 
-    driver = webdriver.Chrome("./chromedriver", options=chrome_options)  # 옵션 적용
+        driver = webdriver.Chrome("./chromedriver", options=chrome_options)  # 옵션 적용
 
-    for company in companies:
-        for keyword_list in keywords:
-            for keyword in keyword_list:
-                print(f"{company} 크롤링 시작.")
+        for company in companies:
+            for keyword_list in keywords:
+                for keyword in keyword_list:
+                    print(f"{company} 크롤링 시작.")
 
-                url = base_url.format(company=company, keyword=keyword) #startDate=yesterday_str, endDate=today.strftime("%Y-%m-%d"))
-                driver.get(url)
+                    url = base_url.format(company=company, keyword=keyword) #startDate=yesterday_str, endDate=today.strftime("%Y-%m-%d"))
+                    driver.get(url)
 
-                # 더보기 버튼 클릭
-                try:
-                    more_button = WebDriverWait(driver, 10).until(
-                        EC.presence_of_element_located((By.CSS_SELECTOR, ".btn_area > button"))
-                    )
-                    more_button.click()
-                except:
-                    print(f"{company} 페이지가 더 이상 없습니다.")
-
-                # 크롤링
-                soup = BeautifulSoup(driver.page_source, "html.parser")
-                news_items = soup.select("div.result_news_wrap ul.news_list li")
-
-                print(f"news_items length: {len(news_items)}")  # 추가
-
-                if not news_items:
-                    print(f"이 기업의 뉴스가 없습니다: {company}.")
-                    #continue
-
-                for news_item in news_items:
-                    title_tag = news_item.select_one("h3.news_ttl")
-                    if title_tag is None:
-                        print("제목이 포함된 뉴스가 없습니다.")
-                        continue
-
-                    link_tag = news_item.select_one("a.news_item")
-                    if link_tag is None:
-                        print("링크가 포함된 뉴스가 없습니다.")
-                        continue
-
-                    
-                    driver.get(link_tag['href'])# 링크 방문
-                    content_soup = BeautifulSoup(driver.page_source, "html.parser")
-                    
-                    content_tag = content_soup.select_one(".news_cnt_detail_wrap")# 뉴스 내용 찾기
-                    if content_tag is None:
-                        print("뉴스 내용이 없습니다.")
-                        continue
-
-                    
-                    
-                    image_tags = content_soup.select(".news_cnt_detail_wrap img")
-
-                    date_tags = content_soup.select_one("dl.registration dd")
-
-
-                    image_urls = []# 이미지 URL을 저장할 리스트 생성
-
-                    if date_tags is not None:
-                        date_str = date_tags.text.strip()
-                    else:
-                        print("날짜 정보가 없습니다.")
-
-                    date = datetime.strptime(date_str,"%Y-%m-%d %H:%M:%S")
-
-                    for image_tag in image_tags: # 이미지 태그에서 src 속성을 가져와서 리스트에 추가
-                        image_url = image_tag["src"]
-                        image_urls.append(image_url)
-
-                    
-                    #print("이미지 URL 리스트:", image_urls)# 이미지 URL 리스트 출력
-
-                    img = image_urls
-                    #content = content_tag.get_text(strip=True)  # 내용 태그에서 내용 text만 가져옴.
-                    title = title_tag.text.strip() #제목 태그 잘라서 제목 text만 가져옴.
-                    print(title)
-                    link = link_tag["href"] #링크 태그 잘라서 링크만 가져옴.
-
-
-                    translated_title = translator.translate(title,dest='en').text
-
-                    # 뉴스 내용 요약
-                    article = Article(link_tag["href"])
+                    # 더보기 버튼 클릭
                     try:
-                        article.download()
-                        article.parse()
-                        article.nlp()
-                    except ArticleException:
-                        print(f"요약에 실패했습니다. URL: {article} ")
-                        continue
-                    summary = article.summary
+                        more_button = WebDriverWait(driver, 10).until(
+                            EC.presence_of_element_located((By.CSS_SELECTOR, ".btn_area > button"))
+                        )
+                        more_button.click()
+                    except:
+                        print(f"{company} 페이지가 더 이상 없습니다.")
 
-                    translated_content = translator.translate(summary, dest='en').text
+                    # 크롤링
+                    soup = BeautifulSoup(driver.page_source, "html.parser")
+                    news_items = soup.select("div.result_news_wrap ul.news_list li")
 
-                    mkDB=Mkcrawling(title=translated_title, 
-                                    news_date=date, 
-                                    link=link, 
-                                    news_agency="매일경제", 
-                                    en_content=translated_content, 
-                                    img=img, 
-                                    collect_date=datetime.now(),
-                                    kr_content=summary)
-                    mkDB.save()
+                    print(f"news_items length: {len(news_items)}")  # 추가
 
-                    driver.back()
+                    if not news_items:
+                        print(f"이 기업의 뉴스가 없습니다: {company}.")
+                        #continue
 
-        print(f"이 기업의 크롤링이 완료 되었습니다. {company}")
+                    for news_item in news_items:
+                        title_tag = news_item.select_one("h3.news_ttl")
+                        if title_tag is None:
+                            print("제목이 포함된 뉴스가 없습니다.")
+                            continue
 
-    print("크롤링 완료.")
+                        link_tag = news_item.select_one("a.news_item")
+                        if link_tag is None:
+                            print("링크가 포함된 뉴스가 없습니다.")
+                            continue
 
+                        
+                        driver.get(link_tag['href'])# 링크 방문
+                        content_soup = BeautifulSoup(driver.page_source, "html.parser")
+                        
+                        content_tag = content_soup.select_one(".news_cnt_detail_wrap")# 뉴스 내용 찾기
+                        if content_tag is None:
+                            print("뉴스 내용이 없습니다.")
+                            continue
+
+                        
+                        
+                        image_tags = content_soup.select(".news_cnt_detail_wrap img")
+
+                        date_tags = content_soup.select_one("dl.registration dd")
+
+
+                        image_urls = []# 이미지 URL을 저장할 리스트 생성
+
+                        if date_tags is not None:
+                            date_str = date_tags.text.strip()
+                        else:
+                            print("날짜 정보가 없습니다.")
+
+                        date = datetime.strptime(date_str,"%Y-%m-%d %H:%M:%S")
+
+                        for image_tag in image_tags: # 이미지 태그에서 src 속성을 가져와서 리스트에 추가
+                            image_url = image_tag["src"]
+                            image_urls.append(image_url)
+
+                        
+                        #print("이미지 URL 리스트:", image_urls)# 이미지 URL 리스트 출력
+
+                        img = image_urls
+                        #content = content_tag.get_text(strip=True)  # 내용 태그에서 내용 text만 가져옴.
+                        title = title_tag.text.strip() #제목 태그 잘라서 제목 text만 가져옴.
+                        print(title)
+                        link = link_tag["href"] #링크 태그 잘라서 링크만 가져옴.
+
+                        try:
+                            existing_link = Mkcrawling.objects.get(link=link)
+                        except ObjectDoesNotExist:
+                            existing_link = None
+
+
+                        translated_title = translator.translate(title,dest='en').text
+
+                        if existing_link is None:
+
+                            # 뉴스 내용 요약
+                            article = Article(link_tag["href"])
+                            try:
+                                article.download()
+                                article.parse()
+                                article.nlp()
+                            except ArticleException:
+                                print(f"요약에 실패했습니다. URL: {article} ")
+                                continue
+                            summary = article.summary
+
+                            translated_content = translator.translate(summary, dest='en').text
+
+                            mkDB=Mkcrawling(title=translated_title, 
+                                            news_date=date, 
+                                            link=link, 
+                                            news_agency="매일경제", 
+                                            en_content=translated_content, 
+                                            img=img, 
+                                            collect_date=datetime.now(),
+                                            kr_content=summary)
+                            mkDB.save()
+
+                            driver.back()
+
+            print(f"이 기업의 크롤링이 완료 되었습니다. {company}")
+
+        print("크롤링 완료.")
+
+schedule.every().day.at("00:00").do(start_mk2)
+
+while True:
+    schedule.run_pending()
+
+    time.sleep(1)
 
 class Command(BaseCommand):
     help = 'Crawl news articles from Maeil Business Newspaper'
--- BE/cominfoApp/navercrawling2.py
+++ BE/cominfoApp/navercrawling2.py
@@ -8,141 +8,145 @@ from datetime import datetime,timedelta
 from newspaper import Article
 from newspaper.article import ArticleException
 from googletrans import Translator
+from django.core.exceptions import ObjectDoesNotExist
 from .models import Crawling
 import requests
+import schedule
 import time
 
 
+def first_day_of_month():
+    return datetime.now().day == 1
+
 def start_navercrawling2():
-    translator = Translator()
-    companies = ["넷플릭스", "삼성전자", "LG전자"]
-    keywords = [["경찰조사수사","검찰조사수사","횡령배임","증여","사기","자금세탁"], 
-                ["법원소송","법원판결","공정거래위원회처분시정제재","금융감독원처분행정지도"], 
-                ["친환경에너지기술","탄소배출권","윤리경영","사회공헌","기업지배구조"]]
+    if first_day_of_month():
+        translator = Translator()
+        companies = ["넷플릭스", "삼성전자", "LG전자"]
+        keywords = [["경찰조사수사","검찰조사수사","횡령배임","증여","사기","자금세탁"], 
+                    ["법원소송","법원판결","공정거래위원회처분시정제재","금융감독원처분행정지도"], 
+                    ["친환경에너지기술","탄소배출권","윤리경영","사회공헌","기업지배구조"]]
 
-    #base_url = "https://search.naver.com/search.naver?where=news&sm=tab_jum&query={company}{keyword}"#&dateType=1day&startDate={startDate}&endDate={endDate}
-    base_url2 = "https://search.naver.com/search.naver?where=news&sm=tab_pge&query={company}{keyword}&sort=0&photo=0&field=0&pd=0&ds=&de=&cluster_rank=457&mynews=0&office_type=0&office_section_code=0&news_office_checked=&nso=so:r,p:all,a:all&start={page}"
+        #base_url = "https://search.naver.com/search.naver?where=news&sm=tab_jum&query={company}{keyword}"#&dateType=1day&startDate={startDate}&endDate={endDate}
+        base_url2 = "https://search.naver.com/search.naver?where=news&sm=tab_pge&query={company}{keyword}&sort=0&photo=0&field=0&pd=0&ds=&de=&cluster_rank=457&mynews=0&office_type=0&office_section_code=0&news_office_checked=&nso=so:r,p:all,a:all&start={page}"
 
-    chrome_options = webdriver.ChromeOptions()
-    chrome_options.add_argument("user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/89.0.4389.82 Safari/537.36")
-    chrome_options.add_argument('--ignore-certificate-errors')  # 인증서 오류 무시 옵션 추가
+        chrome_options = webdriver.ChromeOptions()
+        chrome_options.add_argument("user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/89.0.4389.82 Safari/537.36")
+        chrome_options.add_argument('--ignore-certificate-errors')  # 인증서 오류 무시 옵션 추가
 
-    driver = webdriver.Chrome("./chromedriver", options=chrome_options)  # 옵션 적용
+        driver = webdriver.Chrome("./chromedriver", options=chrome_options)  # 옵션 적용
 
-    #headers = {"User-Agent":"Mozilla/5.0"}
+        #headers = {"User-Agent":"Mozilla/5.0"}
 
-    for company in companies:
-        for keyword_list in keywords:
-            for keyword in keyword_list:
-                print(f"{company}{keyword} 크롤링 시작.")
+        for company in companies:
+            for keyword_list in keywords:
+                for keyword in keyword_list:
+                    print(f"{company}{keyword} 크롤링 시작.")
 
-                page = 1
-                while True:
-                    url = base_url2.format(company=company, keyword=keyword, page=page)
-                    driver.get(url)
-                    article_soup = BeautifulSoup(driver.page_source, "lxml")
-                    news_items = article_soup.select("div.news_area")
+                    page = 1
+                    while True:
+                        url = base_url2.format(company=company, keyword=keyword, page=page)
+                        driver.get(url)
+                        article_soup = BeautifulSoup(driver.page_source, "lxml")
+                        news_items = article_soup.select("div.news_area")
 
-                    if len(news_items) == 0:
-                        print(f"{company}{keyword}에 대한 뉴스 기사가 없습니다.")
-                        break
+                        if len(news_items) == 0:
+                            print(f"{company}{keyword}에 대한 뉴스 기사가 없습니다.")
+                            break
 
-                    print(url)
+                        print(url)
 
-                    print(f"뉴스 개수: {len(news_items)}")
+                        print(f"뉴스 개수: {len(news_items)}")
 
-                    # 여기서부터 뉴스의 제목, 링크, 요약 내용 등을 원하는 방식으로 크롤링하면 됩니다.
-                    # 아래 코드는 뉴스의 제목과 링크를 크롤링하는 예시입니다.
-                    for news_item in news_items:
-                        press_info = news_item.select_one("div.info_group > span.info.press")
-
-                        if press_info is None:
+                        # 여기서부터 뉴스의 제목, 링크, 요약 내용 등을 원하는 방식으로 크롤링하면 됩니다.
+                        # 아래 코드는 뉴스의 제목과 링크를 크롤링하는 예시입니다.
+                        for news_item in news_items:
                             press_info = news_item.select_one("div.info_group > span.info.press")
-                        article = news_item.select_one("div.news_wrap api_ani_send")
 
-                        # press = press_info.text.replace("언론사 선정", "")
-                        # print(press)
-                        title = news_item.select_one(".news_tit").text
-                        link = news_item.select_one(".news_tit")['href']
+                            if press_info is None:
+                                press_info = news_item.select_one("div.info_group > span.info.press")
+                            article = news_item.select_one("div.news_wrap api_ani_send")
+                            title = news_item.select_one(".news_tit").text
+                            link = news_item.select_one(".news_tit")['href']
 
-                        # title = article.get('title')
-                        # link = article.get('href')
-
-                        #content_tag = news_item.select_one("article.story-news article")
-                        date_tag = news_item.select_one("span.info").get_text()
-
-
-                        if '일 전' in date_tag:
-                            days_ago = int(date_tag.split('일 전')[0])
-                            news_date = datetime.now() - timedelta(days=days_ago)
-                        else:
-                            news_date = datetime.now()
-
-                            print(f"Title: {title}")
-                            print(f"Link: {link}")
-                            print("------------")
-                        # 언론사 이름 크롤링
-                        news_agency_tag = news_item.select_one('div.news_info div.info_group a.info.press')
-                        news_agency = news_agency_tag.get_text() if news_agency_tag else None
-                        #print(news_agency)
-
-                        content_soup = BeautifulSoup(driver.page_source, "html.parser")
-                        img_src = content_soup.select_one("a.dsc_thumb img.thumb.api_get")
-                        img = img_src['src'] if img_src else None
-                        #print(img)
-                        #print(content_soup)
+                            try: #link가 이미 DB에 있는지 확인함.
+                                existing_link = Crawling.objects.get(link=link)
+                            except ObjectDoesNotExist:
+                                existing_link = None
 
 
-                        # link_tags = news_item.select("div.news_info div.info_group a")
-                        # print(link_tags)
-                        # for link in link_tags:
-                        #     relative_url = link.get('href')
-                        #     if relative_url:
-                        #         driver.get(relative_url)
-                        #     else:
-                        #         print("링크가 포함된 뉴스가 없습니다.")
-                        
+                            #content_tag = news_item.select_one("article.story-news article")
+                            date_tag = news_item.select_one("span.info").get_text()
 
-                        # 요약 생성
-                        article = Article(link)
-                        try:
-                            article.download()
-                            article.parse()
-                            article.nlp()
-                        except ArticleException:
-                            print(f"요약에 실패했습니다. : {link}")
-                            continue
 
-                        summary = article.summary
-                        #print(f"내용:{summary}")
-                        if not summary:
-                            print(f"{link}에서 내용을 찾을 수 없습니다.")
-                            continue
+                            if '일 전' in date_tag:
+                                days_ago = int(date_tag.split('일 전')[0])
+                                news_date = datetime.now() - timedelta(days=days_ago)
+                            else:
+                                news_date = datetime.now()
 
-                        try:
-                            translated_summary = translator.translate(summary, dest='en').text
-                        except TypeError:
-                            print("이 요약을 번역하지 못했습니다. 다음 뉴스로 이동합니다.")
-                            continue  
+                                print(f"Title: {title}")
+                                print(f"Link: {link}")
+                                print("------------")
+                            # 언론사 이름 크롤링
+                            news_agency_tag = news_item.select_one('div.news_info div.info_group a.info.press')
+                            news_agency = news_agency_tag.get_text() if news_agency_tag else None
+                            #print(news_agency)
 
-                        try:
-                            translated_title = translator.translate(title,dest='en').text
-                        except TypeError:
-                            print("이 요약을 번역하지 못했습니다. 다음 뉴스로 이동합니다.")
-                            continue 
+                            content_soup = BeautifulSoup(driver.page_source, "html.parser")
+                            img_src = content_soup.select_one("a.dsc_thumb img.thumb.api_get")
+                            img = img_src['src'] if img_src else None
 
-                        naverDB = Crawling(title=translated_title, 
-                                        news_date=news_date, 
-                                        link=link, 
-                                        en_content=translated_summary, 
-                                        img=img, 
-                                        collect_date=datetime.now(),
-                                        news_agency=news_agency,
-                                        kr_content=summary)
-                        naverDB.save()
-                    print(f"{company}{keyword} 크롤링 완료.")
-                    page += 10
-        print("마지막 기사입니다.")
+
+                            # 요약 생성
+                            article = Article(link)
+                            try:
+                                article.download()
+                                article.parse()
+                                article.nlp()
+                            except ArticleException:
+                                print(f"요약에 실패했습니다. : {link}")
+                                continue
+
+                            summary = article.summary
+                            #print(f"내용:{summary}")
+                            if not summary:
+                                print(f"{link}에서 내용을 찾을 수 없습니다.")
+                                continue
+
+                            if existing_link is None:
+                                try:
+                                    translated_summary = translator.translate(summary, dest='en').text
+                                except TypeError:
+                                    print("이 요약을 번역하지 못했습니다. 다음 뉴스로 이동합니다.")
+                                    continue  
+
+                                try:
+                                    translated_title = translator.translate(title,dest='en').text
+                                except TypeError:
+                                    print("이 요약을 번역하지 못했습니다. 다음 뉴스로 이동합니다.")
+                                    continue 
+
+                                naverDB = Crawling(title=translated_title, 
+                                                news_date=news_date, 
+                                                link=link, 
+                                                en_content=translated_summary, 
+                                                img=img, 
+                                                collect_date=datetime.now(),
+                                                news_agency=news_agency,
+                                                kr_content=summary)
+                                naverDB.save()
+                        print(f"{company}{keyword} 크롤링 완료.")
+                        page += 10
+            print("마지막 기사입니다.")
+
+
+
+#매월 1일에 크롤링 작업을 수행하는 스케줄링 설정
+schedule.every().day.at("00:00").do(start_navercrawling2)
+while True:
+    schedule.run_pending() #대기 중인 스케줄링 작업이 있으면 수행함.
+
+    time.sleep(1)
 
 
 class Command(BaseCommand):
